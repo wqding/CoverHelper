259c3522b8e61034a031d6e272b918d83613d635
dennea <dennea1228@gmail.com>
2023-01-31 22:11:17 -0500

    more setup :)

---
+++ backend/Controllers/CoverLetterController.js
@@ -0,0 +1,3 @@
+export const generateCoverLetter = async(req,res) => {
+    res.status(200).json({message: "HelloWorld"});
+}
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { generateCoverLetter } from "./Controllers/CoverLetterController.js";
 
 const app = express();
+const router = express.Router()
+router.post('/', generateCoverLetter)
 
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log('Listening on port 3000!'),
 );
+
+app.use('/generate', router);

e15e1d59593eadca47c77f6f357d412ad167ae97
dennea <dennea1228@gmail.com>
2023-02-19 15:34:24 -0500

    prepare for prod

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { generateCoverLetter } from "./Controllers/CoverLetterController.js";
+import path from 'path';
 
 const app = express();
 
@@ -13,7 +14,19 @@ app.use(cors())
 const router = express.Router()
 router.post('', generateCoverLetter)
 
-app.listen(5001, () =>
+// --------------------------deployment------------------------------
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+  );
+}
+// --------------------------deployment------------------------------
+
+app.listen(process.env.PORT, () =>
   console.log('Listening on port 5001!'),
 );
 

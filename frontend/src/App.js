16c8b209418f35c12260d1d888ee12b5ac704f25
Adam Ding <wqding@uwaterloo.ca>
2023-04-15 22:42:31 -0400

    add react ga into useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import ReactGA from "react-ga4";
 // routes
 import Router from './routes';
@@ -7,10 +8,11 @@ import ThemeProvider from './theme';
 
 // ----------------------------------------------------------------------
 
-const MEASUREMENT_ID = "G-CF5MX1V72X";
-ReactGA.initialize(MEASUREMENT_ID);
-
 export default function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-CF5MX1V72X");
+  }, []);
+
   return (
     <ThemeProvider>
       <Router />
